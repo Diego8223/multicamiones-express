16df6824fc3fd4cd442547210287dbede1c29be0
Diego Ruiz <dianruro8223@hotmail.com>
2025-06-05 07:40:45 -0500

    15 commit

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 5173,
   },
   build: {
-    outDir: 'dist', // CAMBIADO de 'build' a 'dist'
+    outDir: 'build',
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name].[hash].[ext]',
